7c584c2079b8bf406561d0c1983cf1a3f3885a13
Okimoto Takuya／沖本　卓也／ADS <1086631-Z220@jp-global.aisingroup.com>
2025-12-22 18:41:51 +0900

    lua/config/options.luaにpythonインタプリタの場所を明記した。

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,4 @@ vim.opt.number = true -- 行番号を表示
 -- vim.opt.relativenumber = true -- 相対行番号を表示
 vim.opt.mouse = 'a' -- マウス操作を有効化
 vim.opt.clipboard:append('unnamedplus') -- システムクリップボードと連携
+vim.g.python3_host_prog = "C:/pyenv-win-master/pyenv-win/versions/3.14.0rc3/python.exe"
